7f3b127bd1e83ea29c40df477e95062768d3e624
Hsienz <55347238+Hsienz@users.noreply.github.com>
2023-02-21 10:46:26 +0800

    fix error border did not show up

--- src/lib/components/URL.svelte
+++ src/lib/components/URL.svelte
@@ -3,7 +3,7 @@
     export let error:string
 </script>
 <form method="POST" action="/" class="p-6 rounded-xl grid bg-Dark_Violet md:p-10 md:bg-cover md:bg-[url('/images/bg-shorten-desktop.svg')] bg-[url('/images/bg-shorten-mobile.svg')] bg-no-repeat bg-contain bg-right-top gap-4">
-    <input name="url" class={`p-2 rounded-md border-4 border-transparent outline-none w-full md:col-span-9 ${error? "border-Red placeholder:text-Red placeholder:opacity-50 " : ""}`} type="text" placeholder="Shorten a link here..."/>
+    <input name="url" class={`p-2 rounded-md border-4 border-transparent outline-none w-full md:col-span-9 ${error? "!border-Red placeholder:text-Red placeholder:opacity-50 " : ""}`} type="text" placeholder="Shorten a link here..."/>
     {#if error}
         <p class="text-Red text-sm md:row-start-2">{error}</p>
     {/if}
